c96563360c98dcd3b009c8613edf70a6f9731e8f
mehdimez <mahdimez@gmail.com>
2019-12-04 23:30:31 +0100

    Dto and add order

--- src/main/java/com/vapeordie/vapeordie/VapeordieApplication.java
+++ src/main/java/com/vapeordie/vapeordie/VapeordieApplication.java
@@ -1,9 +1,15 @@
 package com.vapeordie.vapeordie;
 
+import com.vapeordie.vapeordie.model.Role;
+import com.vapeordie.vapeordie.model.User;
+import com.vapeordie.vapeordie.service.Account.AccountService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class VapeordieApplication {
@@ -16,5 +22,16 @@ public class VapeordieApplication {
     {
         return new  BCryptPasswordEncoder() ;
     }
+    /*@Bean
+    CommandLineRunner start(AccountService accountService) {
+        return args -> {
+            accountService.saveRole(new Role("USER"));
+            accountService.saveRole(new Role("ADMIN"));
+            Stream.of("user1@gmail", "user2@gmail", "user3@gmail", "admin@gmail.com").forEach(r -> {
+                accountService.saveUser(new User("aziz", "bourguiba",r,"123456", null, null));
+
+            });
+        };
+    }*/
 
 }
---
+++ src/main/java/com/vapeordie/vapeordie/dto/OrderProductDto.java
@@ -0,0 +1,59 @@
+package com.vapeordie.vapeordie.dto;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+import java.util.List;
+
+public class OrderProductDto
+{
+
+    public long userId ;
+    public String status;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    // Allows dd/MM/yyyy date to be passed into GET request in JSON
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    public Date date;
+    public List<ProductQteDto> products;
+
+
+    public OrderProductDto(){}
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<ProductQteDto> getProductsId() {
+        return products;
+    }
+
+    public void setProductsId(List<ProductQteDto> products) {
+        this.products = products;
+    }
+
+    public OrderProductDto(long userId, String status, Date date, List<ProductQteDto> products) {
+        this.userId = userId;
+        this.status = status;
+        this.date = date;
+        this.products = products;
+    }
+}
